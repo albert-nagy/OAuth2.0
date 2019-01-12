37f13ab301461dbe9166895388e75f9b1939c497
Albert Nagy <nagy.albert@hotmail.com>
2019-01-12 22:09:05 +0100

    Add FB Login

--- project.py
+++ project.py
@@ -106,6 +106,8 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
+
+    login_session['provider'] = 'google'
     try:
     	login_session['username'] = data['name']
     except KeyError:
@@ -130,6 +132,78 @@ def gconnect():
     print "done!"
     return output
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
+
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v3.2/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v3.2/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v3.2/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+
 @app.route('/gdisconnect')
 def gdisconnect():
 	access_token = login_session.get('access_token')
@@ -143,12 +217,6 @@ def gdisconnect():
 		print 'result is '
 		print result
 		if result['status'] == '200':
-			del login_session['access_token']
-			del login_session['user_id']
-			del login_session['gplus_id']
-			del login_session['username']
-			del login_session['email']
-			del login_session['picture']
 			response = make_response(json.dumps('Successfully disconnected.'), 200)
 			response.headers['Content-Type'] = 'application/json'
 			return response
@@ -160,6 +228,37 @@ def gdisconnect():
 	response = make_response(json.dumps('Current user not connected.'), 401)
 	response.headers['Content-Type'] = 'application/json'
 	return response
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
+
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['access_token']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        response = "You have successfully been logged out."
+    else:
+    	response = "You have successfully been logged out."
+    flash(response)
+    return response
 
 def createUser(login_session):
 	session = DBSession()
@@ -195,24 +294,27 @@ def restaurantMenuJSON(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 	items = session.query(MenuItem).filter_by(
         restaurant_id=restaurant_id).all()
+	response = jsonify(MenuItems=[i.serialize for i in items])
 	session.close()
-	return jsonify(MenuItems=[i.serialize for i in items])
+	return response
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
 	session = DBSession()
 	Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+	response = jsonify(Menu_Item=Menu_Item.serialize)
 	session.close()
-	return jsonify(Menu_Item=Menu_Item.serialize)
+	return response
 
 
 @app.route('/restaurant/JSON')
 def restaurantsJSON():
 	session = DBSession()
 	restaurants = session.query(Restaurant).all()
+	response = jsonify(restaurants=[r.serialize for r in restaurants])
 	session.close()
-	return jsonify(restaurants=[r.serialize for r in restaurants])
+	return response
 
 
 # Show all restaurants
@@ -253,7 +355,6 @@ def editRestaurant(restaurant_id):
 	session = DBSession()
 	editedRestaurant = session.query(
         Restaurant).filter_by(id=restaurant_id).one()
-	session.close()
 	creator = getUserInfo(editedRestaurant.user_id)
 	if login_session['user_id'] != creator.id:
 		session.close()
@@ -264,8 +365,10 @@ def editRestaurant(restaurant_id):
 		if request.form['name']:
 			editedRestaurant.name = request.form['name']
 			flash('Restaurant Successfully Edited %s' % editedRestaurant.name)
+			session.close()
 			return redirect(url_for('showRestaurants'))
 	else:
+		session.close()
 		return render_template('editRestaurant.html', restaurant=editedRestaurant, login_session=login_session)
 
 
@@ -326,8 +429,8 @@ def newMenuItem(restaurant_id):
                            'price'], course=request.form['course'], restaurant_id=restaurant_id, user_id=restaurant.user_id)
 		session.add(newItem)
 		session.commit()
-		session.close()
 		flash('New Menu %s Item Successfully Created' % (newItem.name))
+		session.close()
 		return redirect(url_for('showMenu', restaurant_id=restaurant_id))
 	else:
 		session.close()
